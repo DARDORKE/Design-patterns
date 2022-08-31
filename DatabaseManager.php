cfe99f48d7533db398d3c9794f627ede2445d3de
Kévy DARDOR <kevy.dardor@gmail.com>
2022-08-31 15:55:00 +0200

    Initial commit

---
+++ DatabaseManager.php
@@ -0,0 +1,18 @@
+//Mettez en place une classe DatabaseManager qui implémente les éléments essentiels de ce design pattern, à savoir :
+//
+//Une propriété instance permettant de stocker l'instance
+//
+//Un constructeur et une méthode de clonage privée
+//
+//Une méthode statique getInstance permettant d'instancier ou de retourner cette instance
+//
+//Une méthode métier connect
+//
+//Vérifiez que votre classe ne peut être instanciée qu'une seule fois.
+
+<?php
+
+class DatabaseManager
+{
+
+}
