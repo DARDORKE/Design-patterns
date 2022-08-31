5c08e9bf096ccc94985a1268046803b2a417524c
Kévy DARDOR <kevy.dardor@gmail.com>
2022-08-31 15:58:29 +0200

    Pattern Singleton

--- DatabaseManager.php
+++ DatabaseManager.php
@@ -14,5 +14,34 @@
 
 class DatabaseManager
 {
+    private static $instance;
 
+    private function __construct()
+    {
+    }
+
+    private function __clone()
+    {
+    }
+
+    public static function getInstance(): self
+    {
+        if (!isset(static::$instance)) {
+            static::$instance = new static;
+        }
+
+        return static::$instance;
+    }
+
+    public function connect(): void
+    {
+        // do stuff
+    }
+}
+
+$firstInstance = DatabaseManager::getInstance();
+$secondInstance = DatabaseManager::getInstance();
+
+if ($firstInstance === $secondInstance) {
+    echo "Il s'agit de la même instance";
 }
